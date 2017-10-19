5d449f6f26c09beb4720db1697e35674beacaa92
lfarrel6 <lfarrel6@tcd.ie>
2017-10-19 19:40:11 +0100

    Typer.js greeting message added

---
+++ index.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+var typerString = {
+	strings: ["Hello. ^100 My name is Liam.",
+	"This is my page. ^100 Have a look at some of my projects.",
+	"Enjoy."],
+	typeSpeed: 40
+}
+
+var typed = new Typed("#h-greeting", typerString);
