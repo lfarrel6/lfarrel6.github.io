ed8848774e1c17d08b57e9e8f09ae255bfb087f1
lfarrel6 <lfarrel6@tcd.ie>
2018-01-13 21:27:05 +0000

    Complete Overhaul

--- index.js
+++
@@ -1,8 +0,0 @@
-var typerString = {
-	strings: ["Hello. ^100 My name is Liam.",
-	"This is my page. ^100 Have a look at some of my projects.",
-	"Enjoy."],
-	typeSpeed: 40
-}
-
-var typed = new Typed("#h-greeting", typerString);
